ec50469e7a93e423dfc3fc14219f2da2342ade9e
lufeil <3510002513@qq.com>
2018-12-17 21:13:24 +0800

    init

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -2,10 +2,10 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute('src', 'images/firefox2.png');
+    if(mySrc === 'test-site/images/firefox-icon.png') {
+      myImage.setAttribute('src', 'test-site/images/firefox2.png');
     } else {
-      myImage.setAttribute('src', 'images/firefox-icon.png');
+      myImage.setAttribute('src', 'test-site/images/firefox-icon.png');
     }
 }
 
